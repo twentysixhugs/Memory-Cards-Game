42aa1291a34a91cfca53dcca5947f44cc731870b
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-01 21:42:43 +0300

    Change imports order in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import './index.css';
 import App from './App';
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
+import './styles/reset.css';
+import './styles/index.css';
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 
