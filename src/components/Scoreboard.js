60ea4f156b7cf9e5486854d38615b08c8c33ded9
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-02 16:46:28 +0300

    Add Scoreboard component

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,1 +1,10 @@
-export default function Scoreboard(props) {}
+export default function Scoreboard({ score, highScore }) {
+  return (
+    <aside className="c-scoreboard">
+      <span className="c-scoreboard__score">Score: {score}</span>
+      <span className="c-scoreboard__high-score">
+        High score: {highScore}
+      </span>
+    </aside>
+  );
+}
