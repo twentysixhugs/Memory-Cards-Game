ac583a4a0bc022dc93f27ce1305951db16efd1cf
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-02 17:24:15 +0300

    Add component for game result

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,1 +1,16 @@
-export default function Result(props) {}
+export default function Result({ score, highScore, onPlayAgain }) {
+  return (
+    <div className="c-result">
+      <span className="c-result__text c-result--message">
+        {"Oops, you've already clicked that one"}
+      </span>
+      <span className="c-result__text c-result--score">
+        Score: {score}
+      </span>
+      <span className="c-result__text c-result--score">
+        High score: {highScore}
+      </span>
+      <button onClick={onPlayAgain}>Play again</button>
+    </div>
+  );
+}
