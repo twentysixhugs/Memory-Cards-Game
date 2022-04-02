e94f20c58ba0ad306f9dca9f7db75bba7aff630d
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-02 16:45:32 +0300

    Remove strictmode

--- src/index.js
+++ src/index.js
@@ -6,8 +6,4 @@ import './styles/index.css';
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<App />);
