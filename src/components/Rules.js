e46b0ca9295c6b7c2040e1509a6d6c2baa7e42bf
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-02 17:34:07 +0300

    Write skeleton for Rules

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,1 +1,10 @@
-export default function Rules(props) {}
+export default function Rules({ onButtonClick }) {
+  return (
+    <div className="c-rules">
+      <h3 className="c-rules__heading">Game rules</h3>
+      <button onClick={onButtonClick} className="c-rules__button">
+        OK
+      </button>
+    </div>
+  );
+}
