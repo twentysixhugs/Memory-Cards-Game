ed7805c3e7fb4c15f5cf8a98333f68837ff8f0dc
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-02 16:45:07 +0300

    Write logic for difficulty

--- src/App.js
+++ src/App.js
@@ -1,109 +1,134 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 
 import Difficulty from './components/Difficulty';
 import Result from './components/Result';
 import Rules from './components/Rules';
 import Scoreboard from './components/Scoreboard';
-import Card from './components/Card';
+import CardsWrapper from './components/CardsWrapper';
 
 import shuffleArray from './modules/shuffle';
+import getInitialCards from './modules/getInitialCards';
 
 import './styles/App.css';
 
 export default function App() {
-  const [cards, setCards] = useState([
-    { text: '1 card', id: 1, clicked: false },
-    { text: '2 card', id: 2, clicked: false },
-    { text: '3 card', id: 3, clicked: false },
-    { text: '4 card', id: 4, clicked: false },
-    { text: '5 card', id: 5, clicked: false },
-    { text: '6 card', id: 6, clicked: false },
-    { text: '7 card', id: 7, clicked: false },
-    { text: '8 card', id: 8, clicked: false },
-    { text: '9 card', id: 9, clicked: false },
-    { text: '10 card', id: 10, clicked: false },
-    { text: '11 card', id: 11, clicked: false },
-    { text: '12 card', id: 12, clicked: false },
-    { text: '13 card', id: 13, clicked: false },
-    { text: '14 card', id: 14, clicked: false },
-    { text: '15 card', id: 15, clicked: false },
-    { text: '16 card', id: 16, clicked: false },
-    { text: '17 card', id: 17, clicked: false },
-    { text: '18 card', id: 18, clicked: false },
-    { text: '19 card', id: 19, clicked: false },
-    { text: '20 card', id: 20, clicked: false },
-  ]);
-
-  const [level, setLevel] = useState(1);
+  const [cards, setCards] = useState([...getInitialCards()]);
 
   useEffect(() => {
-    shuffleCards();
+    setCardsForDifficulty(difficulty);
+    setCards((cards) => shuffleArray([...cards]));
   }, []);
 
-  function shuffleCards() {
-    setCards((cards) => shuffleArray([...cards]));
-  }
+  const [difficulty, setDifficulty] = useState(1);
 
-  function getCardsInRange(from, to) {
-    return cards.slice(from, to);
-  }
+  useEffect(() => {
+    if (difficulty !== 1) {
+      setCardsForDifficulty(difficulty);
+    }
+  }, [difficulty]);
 
-  function handleClickOnCard(cardId, e) {
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    if (score > highScore) {
+      setHighScore(score);
+    }
+  }, [score]);
+
+  const [highScore, setHighScore] = useState(0);
+
+  function setCardsForDifficulty(difficulty) {
+    /* Shuffle the existing cards,
+    get the ones without difficulty,
+    get first N of them */
+
+    const chosenCards = shuffleArray(
+      getInitialCards().filter((card) => !card.difficulty),
+    ).splice(0, difficulty * 5);
+
+    /* Get the indexes in the original cards array 
+    which will be replaced with new cards */
+    const indexesInOriginalCards = [];
+
+    chosenCards.forEach((chosenCard) => {
+      const chosenCardIndex = cards.findIndex(
+        (originalCard) => originalCard.id === chosenCard.id,
+      );
+      indexesInOriginalCards.push(chosenCardIndex);
+    });
+
     setCards((cards) =>
-      cards.map((card) => {
-        if (card.id === cardId) {
-          return {
-            ...card,
-            clicked: true,
-          };
+      cards.map((card, index) => {
+        if (indexesInOriginalCards.includes(index)) {
+          return { ...card, difficulty };
         }
 
         return card;
       }),
     );
-
-    shuffleCards();
   }
 
-  // randomly choose N cards for M level
+  function handleClickOnCard(cardId, e) {
+    /* Shuffle on click only if the card
+    is not yet clicked */
+    let isAlreadyClicked = cards.find(
+      (card) => card.id === cardId,
+    ).isClicked;
+
+    if (isAlreadyClicked) {
+      console.log('game over');
+      unmountCards();
+      setScore(0);
+    } else {
+      setScore(score + 1);
+
+      setCards((cards) => shuffleArray([...cards]));
+
+      /* Mark card as clicked */
+      setCards((cards) =>
+        cards.map((card) => {
+          if (card.id === cardId) {
+            return {
+              ...card,
+              isClicked: true,
+            };
+          }
+
+          return card;
+        }),
+      );
+    }
+  }
+
+  function increaseDifficulty() {
+    setCards(() => [...getInitialCards()]); // reset cards
+    setCards((cards) => shuffleArray([...cards])); // shuffle them
+    setDifficulty((difficulty) => difficulty + 1); // increase difficulty
+  }
+
+  function mountCards() {
+    setCards(() => [...getInitialCards()]);
+  }
+
+  function unmountCards() {
+    setCards(() => []);
+  }
+
+  function handlePlayAgain(e) {
+    mountCards();
+    setCardsForDifficulty(1);
+    setDifficulty(1);
+  }
 
   return (
     <div className="App">
-      <h1>Memory Card Game</h1>
-      {level >= 1 && (
-        <div className="cards-wrapper">
-          {getCardsInRange(0, 5).map((card) => (
-            <Card
-              key={card.id}
-              text={card.text}
-              onClick={(e) => handleClickOnCard(card.id, e)}
-            />
-          ))}
-        </div>
-      )}
-      {level >= 2 && (
-        <div className="cards-wrapper">
-          {getCardsInRange(5, 10).map((card) => (
-            <Card
-              key={card.id}
-              text={card.text}
-              onClick={(e) => handleClickOnCard(card.id, e)}
-            />
-          ))}
-        </div>
-      )}
-      {level >= 3 && (
-        <div className="cards-wrapper">
-          {getCardsInRange(10, 15).map((card) => (
-            <Card
-              key={card.id}
-              text={card.text}
-              onClick={(e) => handleClickOnCard(card.id, e)}
-            />
-          ))}
-        </div>
-      )}
-      <button onClick={() => setLevel(level + 1)}>Level + 1</button>
+      <main className="main">
+        <h1>Memory Card Game</h1>
+        <h2>Current difficulty: {difficulty}</h2>
+        <CardsWrapper cards={cards} onCardClick={handleClickOnCard} />
+      </main>
+      <Scoreboard score={score} highScore={highScore} />
+      <button onClick={handlePlayAgain}>Play again</button>
     </div>
   );
 }
