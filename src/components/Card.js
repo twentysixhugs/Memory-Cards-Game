f65a5a6172e57e2bd033e8864597637716967929
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-02 16:45:58 +0300

    Destructure props in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
-export default function Card(props) {
+export default function Card({ onClick, isClicked, text }) {
   return (
-    <div className="c-card" onClick={props.onClick}>
+    <div className="c-card" onClick={onClick}>
       <img className="c-card__img" alt="Card image"></img>
-      <p className="c-card__text">{props.text}</p>
+      <p className="c-card__text">{text}</p>
+      {isClicked ? 'YES' : 'NO'}
     </div>
   );
 }
