f890f241a92fe4ce31be49f70d1e765484efe708
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-02 16:46:20 +0300

    Place initial cards object in a module

---
+++ src/modules/getInitialCards.js
@@ -0,0 +1,19 @@
+export default function getInitialCards() {
+  return [
+    { text: '1 card', id: 1, isClicked: false, difficulty: null },
+    { text: '2 card', id: 2, isClicked: false, difficulty: null },
+    { text: '3 card', id: 3, isClicked: false, difficulty: null },
+    { text: '4 card', id: 4, isClicked: false, difficulty: null },
+    { text: '5 card', id: 5, isClicked: false, difficulty: null },
+    { text: '6 card', id: 6, isClicked: false, difficulty: null },
+    { text: '7 card', id: 7, isClicked: false, difficulty: null },
+    { text: '8 card', id: 8, isClicked: false, difficulty: null },
+    { text: '9 card', id: 9, isClicked: false, difficulty: null },
+    { text: '10 card', id: 10, isClicked: false, difficulty: null },
+    { text: '11 card', id: 11, isClicked: false, difficulty: null },
+    { text: '12 card', id: 12, isClicked: false, difficulty: null },
+    { text: '13 card', id: 13, isClicked: false, difficulty: null },
+    { text: '14 card', id: 14, isClicked: false, difficulty: null },
+    { text: '15 card', id: 15, isClicked: false, difficulty: null },
+  ];
+}
