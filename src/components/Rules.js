9e30f03fbdb983041ba4268e6a66c78820fb9aec
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-03 10:23:39 +0300

    Update rules

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -2,6 +2,13 @@ export default function Rules({ onButtonClick }) {
   return (
     <div className="c-rules">
       <h3 className="c-rules__heading">Game rules</h3>
+      <p className="c-rules__text">
+        You are given multiple images of Angry Birds and Angry Birds Star
+        Wars characters. The images get shuffled every time one of them is
+        clicked. You cannot click on any image more than once, otherwise
+        your score resets to zero. The main goal is to get the highest
+        score as possible.
+      </p>
       <button onClick={onButtonClick} className="c-rules__button">
         OK
       </button>
