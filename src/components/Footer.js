09ab2d25ad8dbd5d68a56be2157e455acf2d2ebb
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-03 14:50:24 +0300

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer className="main__footer">
+      <span className="footer__text">
+        Made with love by{' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/twentysixhugs"
+        >
+          Iaan Lamanosau :3
+        </a>
+      </span>
+    </footer>
+  );
+}
