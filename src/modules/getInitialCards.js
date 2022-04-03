e5bce1befaf618aead459fdc58fd14547d009250
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-04-03 10:38:23 +0300

    Add images to cards

--- src/modules/getInitialCards.js
+++ src/modules/getInitialCards.js
@@ -1,19 +1,125 @@
+import baldEagleImg from '../assets/Bald-eagle.png';
+import bombImg from '../assets/Bomb.png';
+import bubblesImg from '../assets/Bubbles.png';
+import chuckImg from '../assets/Chuck.png';
+import corporalImg from '../assets/Corporal.png';
+import ewokImg from '../assets/Ewok.png';
+import halImg from '../assets/Hal.png';
+import matildaImg from '../assets/Matilda.png';
+import obiwanImg from '../assets/Obi-wan.png';
+import redButGirlImg from '../assets/Red-but-girl.png';
+import redImg from '../assets/Red.png';
+import stellaImg from '../assets/Stella.png';
+import terenceImg from '../assets/Terence.png';
+import theBluesImg from '../assets/The-blues.png';
+import vaderImg from '../assets/Vader.png';
+
 export default function getInitialCards() {
   return [
-    { text: '1 card', id: 1, isClicked: false, difficulty: null },
-    { text: '2 card', id: 2, isClicked: false, difficulty: null },
-    { text: '3 card', id: 3, isClicked: false, difficulty: null },
-    { text: '4 card', id: 4, isClicked: false, difficulty: null },
-    { text: '5 card', id: 5, isClicked: false, difficulty: null },
-    { text: '6 card', id: 6, isClicked: false, difficulty: null },
-    { text: '7 card', id: 7, isClicked: false, difficulty: null },
-    { text: '8 card', id: 8, isClicked: false, difficulty: null },
-    { text: '9 card', id: 9, isClicked: false, difficulty: null },
-    { text: '10 card', id: 10, isClicked: false, difficulty: null },
-    { text: '11 card', id: 11, isClicked: false, difficulty: null },
-    { text: '12 card', id: 12, isClicked: false, difficulty: null },
-    { text: '13 card', id: 13, isClicked: false, difficulty: null },
-    { text: '14 card', id: 14, isClicked: false, difficulty: null },
-    { text: '15 card', id: 15, isClicked: false, difficulty: null },
+    {
+      text: 'Bald Eagle',
+      id: 1,
+      isClicked: false,
+      difficulty: null,
+      img: baldEagleImg,
+    },
+    {
+      text: 'Bomb',
+      id: 2,
+      isClicked: false,
+      difficulty: null,
+      img: bombImg,
+    },
+    {
+      text: 'Bubbles',
+      id: 3,
+      isClicked: false,
+      difficulty: null,
+      img: bubblesImg,
+    },
+    {
+      text: 'Chuck',
+      id: 4,
+      isClicked: false,
+      difficulty: null,
+      img: chuckImg,
+    },
+    {
+      text: 'Corporal',
+      id: 5,
+      isClicked: false,
+      difficulty: null,
+      img: corporalImg,
+    },
+    {
+      text: 'Ewok',
+      id: 6,
+      isClicked: false,
+      difficulty: null,
+      img: ewokImg,
+    },
+    {
+      text: 'Hal',
+      id: 7,
+      isClicked: false,
+      difficulty: null,
+      img: halImg,
+    },
+    {
+      text: 'Matilda',
+      id: 8,
+      isClicked: false,
+      difficulty: null,
+      img: matildaImg,
+    },
+    {
+      text: 'Obi-Wan',
+      id: 9,
+      isClicked: false,
+      difficulty: null,
+      img: obiwanImg,
+    },
+    {
+      text: 'Red, but girl',
+      id: 10,
+      isClicked: false,
+      difficulty: null,
+      img: redButGirlImg,
+    },
+    {
+      text: 'Red',
+      id: 11,
+      isClicked: false,
+      difficulty: null,
+      img: redImg,
+    },
+    {
+      text: 'Stella',
+      id: 12,
+      isClicked: false,
+      difficulty: null,
+      img: stellaImg,
+    },
+    {
+      text: 'Terence',
+      id: 13,
+      isClicked: false,
+      difficulty: null,
+      img: terenceImg,
+    },
+    {
+      text: 'The blues',
+      id: 14,
+      isClicked: false,
+      difficulty: null,
+      img: theBluesImg,
+    },
+    {
+      text: 'Vader',
+      id: 15,
+      isClicked: false,
+      difficulty: null,
+      img: vaderImg,
+    },
   ];
 }
